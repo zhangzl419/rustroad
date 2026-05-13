2ffd846db1211340f714825dd504f1e4c0561fc9
zhangzhenglong <zhangzhenglong@bjneweastech.com>
2026-05-13 16:48:06 +0800

    编写第二章

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,27 @@
 
 echo '开始执行命令'
 # 生成静态文件
-echo '执行命令：gitbook build .'
+echo '执行命令: gitbook build .'
 gitbook build .
 
 # 进入生成的文件夹
-echo "执行命令：cd ./_book\n"
+echo "执行命令: cd ./_book\n"
 cd ./_book
 
 # 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
-echo "执行命令：git init\n"
+echo "执行命令: git init\n"
 git init
 
 # 保存所有的修改
-echo "执行命令：git add -A"
+echo "执行命令: git add -A"
 git add -A #整个仓库都加入到index区
 
 # 把修改的文件提交
-echo "执行命令：commit -m 'deploy'"
+echo "执行命令: commit -m 'deploy'"
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-echo "执行命令：git push -f https://github.com/yulilong/book.git master:gh-pages"
+echo "执行命令: git push -f https://github.com/yulilong/book.git master:gh-pages"
 git push -f https://github.com/zhangzl419/rustroad.git master:gh-pages
 
 # 返回到上一次的工作目录
